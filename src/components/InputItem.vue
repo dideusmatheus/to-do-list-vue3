bcfce06a49a730a7b3435ffe7e56fb96842004c4
Matheus de Deus Rocha <matheus.rocha@sis-it.com>
2024-06-07 14:51:37 -0300

    Criando os input para o item

---
+++ src/components/InputItem.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="input-group mb-3">
+        <button class="btn btn-outline-secondary" type="button">
+            <font-awesome-icon :v-if="true" :icon="['fas', 'check']" style="color: #11ff00;"/>
+            <font-awesome-icon :v-else-if="false" :icon="['fas', 'xmark']" style="color: #ff0000;"/>
+        </button>
+        <input type="text" class="form-control" placeholder="" aria-label="Item a fazer">
+        <button class="btn btn-outline-secondary" type="button">
+            <font-awesome-icon :icon="['fas', 'trash']"/>
+        </button>
+    </div>
+</template>
