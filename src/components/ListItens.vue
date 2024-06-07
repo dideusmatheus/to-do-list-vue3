cd5a76f0ec6f2837845bcbaf6c030720d9747b1f
Matheus de Deus Rocha <matheus.rocha@sis-it.com>
2024-06-07 14:52:20 -0300

    Importando o input item no componente de lista dos itens

---
+++ src/components/ListItens.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <InputItem></InputItem>
+    </div>
+</template>
+
+<script>
+    import InputItem from '@/components/InputItem.vue'
+
+    export default {
+        components: {
+            InputItem
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
