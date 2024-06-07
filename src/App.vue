7723e19757f26fe40526239b11c9140b75c2a090
Matheus de Deus Rocha <matheus.rocha@sis-it.com>
2024-06-07 13:43:57 -0300

    Criando o componente Spinner

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div class="row">
+    <div class="col-12 d-flex justify-content-center">
+      <!-- Card -->
+      <div class="card">
+
+        <div class="row">
+
+          <div class="col-12 mb-4">
+            <!-- Spinner -->
+            <SpinnerLoading></SpinnerLoading>
+          </div>
+          
+        </div>
+      </div>
+
+    </div>
+  </div> 
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SpinnerLoading from './components/SpinnerLoading.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SpinnerLoading,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    margin-top: 60px;
+  }
+  .card {
+      border-radius: 10px;
+      box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.1),
+                  -5px -5px 15px rgba(255, 255, 255, 0.7);
+      padding: 20px;
+      text-align: center;
+      justify-content: center;
+      width: 500px;
+      transition: transform 0.3s;
+  }
 </style>
---
+++ src/components/SpinnerLoading.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="spinner-border text-primary" role="status">
+        <span class="visually-hidden">Loading...</span>
+    </div>
+</template>
+
+<style>
+.spinner-border{
+    width: 5rem;
+    height: 5rem;
+}
+</style>
