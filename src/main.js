e8a2d48a4b2c6e625475e5b4d467f294dec8d480
Matheus de Deus Rocha <matheus.rocha@sis-it.com>
2024-06-07 14:48:31 -0300

    Importando Font Awesome

--- src/main.js
+++ src/main.js
@@ -4,4 +4,19 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
 
-createApp(App).use(bootstrap).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* import specific icons */
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+/* add icons to the library */
+library.add(fas, far, fab)
+
+createApp(App)
+  .use(bootstrap)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app')
