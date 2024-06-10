f175eb86366ce3bad41aab099a9f22f22a124a96
Matheus de Deus Rocha <matheus.rocha@sis-it.com>
2024-06-10 09:35:41 -0300

    Adicionando nova tarefa na lista

--- src/components/InputAddForm.vue
+++ src/components/InputAddForm.vue
@@ -1,7 +1,34 @@
 <template>
-    <div class="input-group mb-3">
-        <input type="text" class="form-control" placeholder="Adicionar um nome item..." aria-label="Adicionar um nome item...">
-        <button class="btn btn-outline-primary" type="button" id="button-addon2">Adicionar</button>
-    </div>
+    <form class="input-group mb-3" @submit.stop.prevent="addToDo()">
+        <input type="text" class="form-control" v-model="title" placeholder="Adicionar um nome item..." aria-label="Adicionar um nome item...">
+        <button class="btn btn-outline-primary" type="submit" id="button-addon2">Adicionar</button>
+    </form>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                title: ''
+            }
+        },
+        methods: {
+            addToDo() {
+                if(!this.title){
+                    return;
+                }
+
+                let newToDo = {
+                    title: this.title,
+                    completed: false
+                }
+
+                this.$store.dispatch('addToDo', newToDo)
+                .finally(() => {
+                    this.title = '';
+                })
+            }
+        },
+    }
+</script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -12,15 +12,24 @@ export default createStore({
     mutations: {
         storeTodos(state, payload){
             state.todos = payload;
+        },
+        updateTodos(state, payload){
+            state.todos.unshift(payload);
         }
     },
 
     actions: {
-        getTodos({commit}) {
+        getTodos({ commit }) {
             return axios.get(`${this.state.urlBase}todos`)
                 .then((responseToDos) => {
-                    commit('storeTodos', responseToDos.data)
-                })
+                    commit('storeTodos', responseToDos.data);
+                });
         },
+        addToDo({ commit }, data){
+            return axios.post(`${this.state.urlBase}todos`, data)
+            .then((response) => {
+                commit('updateTodos', response.data);
+            });
+        }
     }
 })
