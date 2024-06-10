ecf746d81759ad7693f29912f4c5a489c8130a2b
Matheus de Deus Rocha <matheus.rocha@sis-it.com>
2024-06-10 11:08:47 -0300

    Atualizando os valores do item

--- src/components/InputItem.vue
+++ src/components/InputItem.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="input-group mb-3">
-        <button class="btn btn-outline-secondary" type="button">
-            <font-awesome-icon v-if="todo.completed" :icon="['fas', 'check']" style="color: #11ff00;"/>
-            <font-awesome-icon v-if="!todo.completed" :icon="['fas', 'xmark']" style="color: #ff0000;"/>
+        <button class="btn btn-outline-secondary" type="button" @click="completed()">
+            <font-awesome-icon v-if="isCompletes" :icon="['fas', 'check']" style="color: #11ff00;"/>
+            <font-awesome-icon v-if="!isCompletes" :icon="['fas', 'xmark']" style="color: #ff0000;"/>
         </button>
-        <input type="text" class="form-control" placeholder="" @keyup.enter="updateTodo($event)" :value="todo.title" aria-label="Item a fazer">
+        <input type="text" class="form-control" placeholder="" @keyup.enter="updateTodo($event)" v-model="title" aria-label="Item a fazer">
         <button class="btn btn-outline-secondary" type="button">
             <font-awesome-icon :icon="['fas', 'trash']"/>
         </button>
@@ -19,22 +19,33 @@
                 default: () => ({})
             },
         },
+        data() {
+            return {
+                title: this.todo.title,
+                isCompletes: this.todo.completed
+            }
+        },
         methods: {
-            updateTodo($event) {
-                if(!$event.target.value){
+           getTitleTodo() {
+                if(!this.title){
                     return;
                 }
 
+                this.updateTodo();
+            },
+            updateTodo(){
                 let payload = {
                     id: this.todo.id,
                     data: {
-                        title: $event.target.value,
-                        completed: this.todo.completed
+                        title: this.title,
+                        completed: this.isCompletes
                     }
                 };
                 this.$store.dispatch('updateToDo', payload);
-
-                console.log('payload' , payload)
+            },
+            completed(){
+                this.isCompletes = !this.isCompletes;
+                this.updateTodo()
             }
         },
     }
